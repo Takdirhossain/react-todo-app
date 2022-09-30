2bbb3b7359d0aebc14c5c0c596f6d25c18c23653
takdirhossain <takdirhossain35@gmail.com>
2022-10-01 02:43:55 +0600

    fix input

--- src/components/TodoFroms.js
+++ src/components/TodoFroms.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 
-const TodoFroms = () => {
+const TodoFroms = (props) => {
     const [input, setInput] = useState('')
 
+    const hamdleChange = e => {
+        setInput(e.target.value)
+    }
     const handaleSubmit = e => {
         e.preventDefault();
+
+      
+        setInput('')
     }
     return (
         <form className='todo-from' onSubmit={handaleSubmit}>
-            <input type="text" placeholder='Add A Todo' name='todo' className='todo-input' value={input} />
+            <input type="text" placeholder='Add A Todo' name='todo' className='todo-input' value={input} onChange={hamdleChange}/>
             <button>Add Todo</button>
         </form>
     );
